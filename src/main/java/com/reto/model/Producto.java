09cf22bba09f4a17b1bc5ffb4144e581b420b99b
alejo lopez <alejitolopez19900109@gmail.com>
2026-09-24 13:00:42 -0500

    no se cual es este commit

---
+++ src/main/java/com/reto/model/Producto.java
@@ -0,0 +1,46 @@
+package com.reto.model;
+
+/**
+ * Representa un producto con nombre, categoria y precio.
+ */
+public class Producto {
+
+    private String nombre;
+    private String categoria;
+    private double precio;
+
+    public Producto(String nombre, String categoria, double precio) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-15s | %-12s | $%,.2f", nombre, categoria, precio);
+    }
+}
